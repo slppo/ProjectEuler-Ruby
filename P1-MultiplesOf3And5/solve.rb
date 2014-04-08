b16e33ad5e0aa9aa4303f4d50eae8e9696545c05
Tyler O'Neill <toneill1995@outlook.com>
2014-04-07 22:50:20 -0500

    add hashbang to solutions

--- P1-MultiplesOf3And5/solve.rb
+++ P1-MultiplesOf3And5/solve.rb
@@ -1,3 +1,5 @@
+#! /usr/bin/env ruby
+
 multiplesOf3 = (1..1000/3).to_a.map { |e| e * 3 }
 multiplesOf5 = (1...1000/5).to_a.map { |e| e * 5 }
 multiples = (multiplesOf3 + multiplesOf5).uniq
--- P2-EvenFibonacci/solve.rb
+++ P2-EvenFibonacci/solve.rb
@@ -1,3 +1,5 @@
+#! /usr/bin/env ruby
+
 $fibTerms = [1, 2]
 
 def fibonacci(n)	
